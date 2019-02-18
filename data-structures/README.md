5888801ad8a578750eebe157d50396edc26923a1
denlysenko <den.lysenko.mail@gmail.com>
2019-02-18 17:51:13 +0200

    BST: insert

--- data-structures/README.md
+++ data-structures/README.md
@@ -6,3 +6,4 @@
 - [Doubly Linked List](https://github.com/denlysenko/algorithms-and-data-structures/tree/master/data-structures/doubly_linked_list.js)
 - [Stack](https://github.com/denlysenko/algorithms-and-data-structures/tree/master/data-structures/stack.js)
 - [Queue](https://github.com/denlysenko/algorithms-and-data-structures/tree/master/data-structures/queue.js)
+- [Binary Search Tree](https://github.com/denlysenko/algorithms-and-data-structures/tree/master/data-structures/binary_search_tree.js)
---
+++ data-structures/binary_search_tree.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (data === current.data) {
+        return undefined;
+      }
+
+      if (data < current.data) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+
+        current = current.right;
+      }
+    }
+  }
+}
