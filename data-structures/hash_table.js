ebd741e3bb8b567b94bce0de35c1839c9ddc2b45
denlysenko <den.lysenko.mail@gmail.com>
2019-03-31 17:07:52 +0300

    hash_table: keys/values

--- data-structures/hash_table.js
+++ data-structures/hash_table.js
@@ -6,7 +6,7 @@ class HashTable {
   set(key, value) {
     const index = this._hash(key);
 
-    if(!this.keyMap[index]) {
+    if (!this.keyMap[index]) {
       this.keyMap[index] = [];
     }
 
@@ -15,12 +15,12 @@ class HashTable {
 
   get(key) {
     const index = this._hash(key);
-    const value = this.keyMap[index];
+    const bucket = this.keyMap[index];
 
-    if(value) {
-      for(let i = 0; i < value.length; i++) {
-        if(value[i][0] === key) {
-          return value[i][1];
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          return bucket[i][1];
         }
       }
     }
@@ -28,11 +28,47 @@ class HashTable {
     return undefined;
   }
 
+  keys() {
+    const result = [];
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      const bucket = this.keyMap[i];
+
+      if (bucket) {
+        for (let j = 0; j < item.length; j++) {
+          if (!result.includes(bucket[j][0])) {
+            result.push(bucket[j][0]);
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+  values() {
+    const result = [];
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      const bucket = this.keyMap[i];
+
+      if (bucket) {
+        for (let j = 0; j < bucket.length; j++) {
+          if (!result.includes(bucket[j][1])) {
+            result.push(bucket[j][1]);
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
   _hash(key) {
     let total = 0;
     const PRIME = 31;
 
-    for(let i = 0; i < Math.min(key.length, 100); i++) {
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
       const char = key[i];
       const value = char.charCodeAt(0) - 96;
       total = (total * PRIME + value) % this.keyMap.length;
