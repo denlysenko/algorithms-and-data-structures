7e251464b639c0b5864141c24c304c0a42cabced
denlysenko <den.lysenko.mail@gmail.com>
2019-04-08 16:47:06 +0300

    remove edge/vertex

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -13,4 +13,21 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      this.removeEdge(vertex, this.adjacencyList[vertex].pop());
+    }
+
+    delete this.adjacencyList[vertex];
+  }
 }
